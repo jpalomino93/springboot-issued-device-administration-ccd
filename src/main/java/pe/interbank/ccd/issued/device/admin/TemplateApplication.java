2389f619f3bd22ab843d0ffe832cba4b009ed2b3
jpalomino93 <jpalomino93@hotmail.com>
2025-12-12 10:06:34 -0500

    quitando muestra credenciales azure

--- src/main/java/pe/interbank/ccd/issued/device/admin/TemplateApplication.java
+++ src/main/java/pe/interbank/ccd/issued/device/admin/TemplateApplication.java
@@ -11,10 +11,4 @@ import pe.interbank.ccd.issued.device.admin.config.CobisProperties;
 @EnableConfigurationProperties(CobisProperties.class)
 public class TemplateApplication {
  public static void main(String[] args){SpringApplication.run(TemplateApplication.class,args);}
- @PostConstruct
- public void debugAzureAuth() {
-  System.out.println("CLIENT_ID = " + System.getenv("AZURE_CLIENT_ID"));
-  System.out.println("TENANT_ID = " + System.getenv("AZURE_TENANT_ID"));
-  System.out.println("CLIENT_SECRET = " + System.getenv("AZURE_CLIENT_SECRET"));
- }
 }
