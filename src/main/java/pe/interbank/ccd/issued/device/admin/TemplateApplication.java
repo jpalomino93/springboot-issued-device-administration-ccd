3b653a00ced1fe17a16db0b0b5c7fa99eb1ac053
jpalomino93 <jpalomino93@hotmail.com>
2025-12-12 10:07:39 -0500

    quitando secrets al MD

--- src/main/java/pe/interbank/ccd/issued/device/admin/TemplateApplication.java
+++ src/main/java/pe/interbank/ccd/issued/device/admin/TemplateApplication.java
@@ -11,4 +11,10 @@ import pe.interbank.ccd.issued.device.admin.config.CobisProperties;
 @EnableConfigurationProperties(CobisProperties.class)
 public class TemplateApplication {
  public static void main(String[] args){SpringApplication.run(TemplateApplication.class,args);}
+ @PostConstruct
+ public void debugAzureAuth() {
+  System.out.println("CLIENT_ID = " + System.getenv("AZURE_CLIENT_ID"));
+  System.out.println("TENANT_ID = " + System.getenv("AZURE_TENANT_ID"));
+  System.out.println("CLIENT_SECRET = " + System.getenv("AZURE_CLIENT_SECRET"));
+ }
 }
